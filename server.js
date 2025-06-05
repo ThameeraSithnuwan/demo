f27166005702531bf577cb03c6e4805950d0851b
ThameeraSithnuwan <149369868+ThameeraSithnuwan@users.noreply.github.com>
2025-06-05 07:02:17 +0530

    Vvvv (#276)
    * Update server.js
    
    * Update Dockerfile

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ app.use(express.json());
 app.get('/', (req, res) => {
   res.send('Hello, world!');
 });
-
+// test
 // Start server
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
